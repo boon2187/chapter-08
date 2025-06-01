34b8fbd91b94dc0f7c1c119c9649a56ae48c9231
boon2187 <fumiya.mai.ito@gmail.com>
2025-06-01 12:48:37 +0900

    記事一覧APIの実装

---
+++ app/api/posts/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// 記事一覧の取得API(GET)
+export const GET = async () => {
+  try {
+    // DBから記事一覧を取得
+    const posts = await prisma.post.findMany({
+      include: {
+        // カテゴリーも取得
+        postCategories: {
+          include: {
+            category: {
+              // select id, name from Category;
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      // 作成日時の降順で取得
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return NextResponse.json({ status: "ok", posts: posts }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 });
+    }
+  }
+};
