78ee62999cf9c2c8d272caf007df9fb99263b73c
boon2187 <fumiya.mai.ito@gmail.com>
2025-06-01 18:17:25 +0900

    記事詳細取得のAPIの実装

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// 記事一覧の取得API(GET)
+export const GET = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const id = parseInt(params.id);
+    // URLパラメータのバリデーション
+    if (isNaN(id)) {
+      return NextResponse.json(
+        { status: "error", message: "Invalid ID format" },
+        { status: 400 }
+      );
+    }
+
+    // DBから記事一覧を取得
+    const post = await prisma.post.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        // カテゴリーも取得
+        postCategories: {
+          include: {
+            category: {
+              // select id, name from Category;
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    // 記事が見つからなかった場合
+    if (!post) {
+      return NextResponse.json(
+        { status: "error", message: "Post not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ status: "ok", post: post }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 });
+    }
+  }
+};
