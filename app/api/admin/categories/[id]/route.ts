80e2167d5c52f10f2897e73a0e16a185a811c731
boon2187 <fumiya.mai.ito@gmail.com>
2025-06-10 08:48:04 +0900

    カテゴリーの更新と削除のAPIを追加

--- app/api/admin/categories/[id]/route.ts
+++ app/api/admin/categories/[id]/route.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// カテゴリの型のinterface
+interface UpdateCategoryRequestBody {
+  name: string;
+}
+
 // カテゴリの詳細取得API(GET)
 export const GET = async (
   request: NextRequest,
@@ -14,6 +19,47 @@ export const GET = async (
       where: { id: Number(id) },
     });
     return NextResponse.json(category, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 });
+    }
+    return NextResponse.json({ status: "Unknown error" }, { status: 500 });
+  }
+};
+
+// カテゴリの更新API(PUT)
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+    const body = await request.json();
+    const { name }: UpdateCategoryRequestBody = body;
+    const category = await prisma.category.update({
+      where: { id: Number(id) },
+      data: { name },
+    });
+    return NextResponse.json(category, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 });
+    }
+    return NextResponse.json({ status: "Unknown error" }, { status: 500 });
+  }
+};
+
+// カテゴリの削除API(DELETE)
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+    await prisma.category.delete({
+      where: { id: Number(id) },
+    });
+    return NextResponse.json({ status: "OK" }, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ status: error.message }, { status: 400 });
