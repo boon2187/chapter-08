c0edbfaa65536b55e120ba2ac490acaf4a216d79
boon2187 <fumiya.mai.ito@gmail.com>
2025-06-11 08:49:10 +0900

    管理者用のフロントページの雛形をまず作成

---
+++ app/admin/categories/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminCategoryEdit() {
+  return <div>AdminCategoryEdit</div>;
+}
---
+++ app/admin/posts/new/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminNewPost() {
+  return <div>AdminNewPost</div>;
+}
